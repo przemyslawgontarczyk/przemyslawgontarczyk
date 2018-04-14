f5c5649f165a2945217f0fd133a3a13785fd6801
“przemyslawgontarczyk <przemyslawgontarczyk@gmail.com>
2018-04-14 02:07:01 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://enigmatic-springs-88547.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://enigmatic-springs-88547.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://sheltered-lowlands-48485.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://sheltered-lowlands-48485.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
